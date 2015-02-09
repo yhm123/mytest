9ec9a69321f199987f626693e090bb03395af814
nizuimeiabc1 <2316475021@qq.com>
2015-02-09 15:31:09 +0800

    update

--- cc/src/main/java/com/ai/redis/FileTest.java
+++ cc/src/main/java/com/ai/redis/FileTest.java
@@ -11,35 +11,99 @@ public class FileTest
     
     /**
      * @Title: main
-     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @Description: TODO(杩欓噷鐢ㄤ竴鍙ヨ瘽鎻忚堪杩欎釜鏂规硶鐨勪綔鐢�
      * @Author: Administrator
-     * @Since: 2014-11-28下午02:53:54
+     * @Since: 2014-11-28涓嬪崍02:53:54
      * @param args
      * @throws IOException
      */
     public static void main(String[] args)
         throws Exception
     {
-        int[] sample = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, -6};
-        List<Integer> list = new ArrayList<Integer>();
-        for (int i : sample)
-            list.add(i);
-        // use the native TimSort in JDK 7
-        Collections.sort(list, new Comparator<Integer>()
+        FileTest test = new FileTest();
+        Person p1 = test.new Person();
+        p1.setAge(5);
+        p1.setGrade(6);
+        Person p2 = test.new Person();
+        p2.setAge(8);
+        p2.setGrade(3);
+        Person p3 = test.new Person();
+        p3.setAge(4);
+        p3.setGrade(7);
+        List<Person> list = new ArrayList<Person>();
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+        for (int i = 0, len = list.size(); i < len; i++)
         {
-            @Override
-            public int compare(Integer o1, Integer o2)
-            {
-                // miss the o1 = o2 case on purpose
-                return o1 > o2 ? 1 : o1 == o2 ? 0 : -1;
-            }
-        });
-        
+            System.out.println(list.get(i));
+        }
+        // use the native TimSort in JDK 7
+        Collections.sort(list, new Compare());
+        for (int i = 0, len = list.size(); i < len; i++)
+        {
+            System.out.println(list.get(i));
+        }
         // List<String> lines = FileUtils.readLines(new File("D:\\test\\0-0-accountdeposit.dat"));
         // FileUtils.writeLines(new File("D:\\test\\b.txt"), "GBK", lines, true);
         // for (String str : lines)
         // {
-        // FileUtils.writeStringToFile(new File("D:\\test\\b.txt"), str, "GBK", true);//只有一行记录
+        // FileUtils.writeStringToFile(new File("D:\\test\\b.txt"), str, "GBK", true);//鍙湁涓�璁板綍
         // }
+    }
+    
+    class Person
+    {
+        private int age;
+        
+        private int grade;
+        
+        public int getAge()
+        {
+            return age;
+        }
+        
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+        
+        public int getGrade()
+        {
+            return grade;
+        }
+        
+        public void setGrade(int grade)
+        {
+            this.grade = grade;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return "age=" + age + ";grade=" + grade;
+        }
+    }
+    
+    static class Compare implements Comparator<Object>
+    {
+        @Override
+        public int compare(Object o1, Object o2)
+        {
+            Person p1 = (Person)o1;
+            Person p2 = (Person)o2;
+            if (p1.getAge() < p2.getAge() && p1.getGrade() < p2.getGrade())
+            {
+                return -1;
+            }
+            else if (p1.getAge() > p2.getAge() && p1.getGrade() > p2.getGrade())
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
